e883d5ef7adcf0c959a14cec829c5b362561f0a1
PinusThunbergii <aleksey.maksymyshyn@gmail.com>
2020-02-24 00:06:07 +0200

    sfaf

--- Classes/StartScene.cpp
+++ Classes/StartScene.cpp
@@ -1,9 +1,22 @@
 #include "StartScene.h"
 
+using namespace cocos2d;
+
 bool StartScene::init()
 {
-    if(!Scene::init()) return false;
-    //auto label = Label::create
+    if(!Scene::init()) 
+    {
+        return false;
+    }
+
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+    auto label = Label::createWithTTF("Tetris", "fonts/Marker Felt.ttf", 24);
+    label->setPosition(Vec2::ZERO);
+    this->addChild(label, 1);
+
+    return true;
 }
 
 Scene* StartScene::createScene()
