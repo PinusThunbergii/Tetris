6b07226691781bc47a2d92f08e812accadb69f8d
PinusThunbergii <aleksey.maksymyshyn@gmail.com>
2020-03-02 22:20:22 +0200

    55525

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -3,7 +3,7 @@
 
 bool GameScene::init()
 {
-    if(!Scene::init())
+    if (!Scene::init())
     {
         return false;
     }
@@ -12,7 +12,6 @@ bool GameScene::init()
     Vec2 center = Vec2(visibleSize.width / 2.0f, visibleSize.height / 2.0f);
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-
     backButton = Button::create("icons/back.png", "icons/back_selected.png");
     backButton->setAnchorPoint(Vec2::ANCHOR_BOTTOM_LEFT);
     backButton->setScale(visibleSize.height / 3000);
@@ -20,10 +19,22 @@ bool GameScene::init()
     backButton->addClickEventListener(CC_CALLBACK_0(onClick_backButton, this));
     this->addChild(backButton, -1);
 
+    box = Sprite::create("sprites/box.png");
+    box->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
+    box->setPosition(center);
+    box->setScale(0.2);
+    this->addChild(box, -1);
+
+    auto eventListenerKeyBoard = EventListenerKeyboard::create();
+    eventListenerKeyBoard->onKeyPressed = CC_CALLBACK_2(GameScene::keyPressed, this);
+    eventListenerKeyBoard->onKeyReleased = CC_CALLBACK_2(GameScene::keyReleased, this);
+    Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(eventListenerKeyBoard, this);
+
+    this->scheduleUpdate();
     return true;
 }
 
-Scene* GameScene::createScene()
+Scene *GameScene::createScene()
 {
     return GameScene::create();
 }
@@ -31,4 +42,44 @@ Scene* GameScene::createScene()
 void GameScene::onClick_backButton(Ref *sender)
 {
     Director::getInstance()->replaceScene(StartScene::create());
+}
+
+void GameScene::keyPressed(EventKeyboard::KeyCode keyCode, Event *event)
+{
+    std::cout << (int)keyCode << std::endl;
+    Vec2 dir;
+    float deltaMove = 50;
+    switch (keyCode)
+    {
+
+    case EventKeyboard::KeyCode::KEY_W:
+        dir = Vec2(0, deltaMove);
+        break;
+    case EventKeyboard::KeyCode::KEY_A:
+        dir = Vec2(-deltaMove, 0);
+        break;
+    case EventKeyboard::KeyCode::KEY_S:
+        dir = Vec2(0, -deltaMove);
+        break;
+    case EventKeyboard::KeyCode::KEY_D:
+        dir = Vec2(deltaMove, 0);
+        break;
+    default:
+        return;
+        //break;
+    }
+    auto mvb = MoveBy::create(0.5, dir);
+    box->runAction(mvb);
+}
+
+void GameScene::keyReleased(EventKeyboard::KeyCode keyCode, Event *event)
+{
+    std::cout << (int)keyCode << std::endl;
+}
+
+void GameScene::update(float dt)
+{
+    std::cout << "Update interval: " << dt << std::endl;
+    Vec2 boxPosition = box->getPosition();
+    //if(boxPosition.x <= 0 || boxPosition.y <= 0)
 }
--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -10,8 +10,12 @@ class GameScene: public Scene
 public:
     virtual bool init();
     static Scene* createScene();
+    void update(float dt);
     CREATE_FUNC(GameScene);
 private:
     Button* backButton;
+    Sprite* box;
     static void onClick_backButton(Ref *sender);
+    void keyPressed(EventKeyboard::KeyCode keyCode, Event* event);
+    void keyReleased(EventKeyboard::KeyCode keyCode, Event* event);
 };
